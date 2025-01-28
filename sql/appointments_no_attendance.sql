65910dac62f1d0d5c9adc7bf4e9a6371132d43cc
manuguth <manuguth>
2025-01-28 20:03:42 +0000

    switching to silver tables

--- sql/appointments_no_attendance.sql
+++ sql/appointments_no_attendance.sql
@@ -5,8 +5,8 @@ WITH leaders AS (
     km_members.mail,
     km_orgs.name as Register
   from
-    bronze.km_roles
-    left JOIN bronze.km_members ON km_roles.kmUserId = km_members.id
+    silver.km_roles
+    left JOIN silver.km_members ON km_roles.kmUserId = km_members.id
     LEFT JOIN silver.km_orgusermapping as km_orgusermapping ON km_orgusermapping.kmUserId = km_roles.kmUserId
     and km_orgusermapping.active = 1
     LEFT JOIN silver.km_orgs as km_orgs ON km_orgusermapping.orgId = km_orgs.id
